12982a6308a16d117064298a4df7fd4e64d8ac70
Iga <iga.naskret@gmail.com>
2019-12-01 14:34:57 +0100

    dziala jak ta lala

--- js.js
+++ js.js
@@ -5,100 +5,125 @@ const arr2 = [0, 1, 2, 3, 4, 5];
 const arr3 = [0, 1, 2, 3, 4, 5];
 
 function randomNumbers() {
-  return Math.floor(Math.random() * 6);
+  return Math.floor(Math.random() * 15);
 }
 
-function populate() {
-  let divOne = document.querySelector(".div1");
+let divOne = document.querySelector(".div1");
+let divTwo = document.querySelector(".div2");
+let divThree = document.querySelector(".div3");
+
+populate(divOne);
+populate(divTwo);
+populate(divThree);
+
+function populate(div) {
   for (let i = 0; i < 6; i++) {
     let span = document.createElement("span");
     span.classList.add("span" + i);
     span.textContent = i;
-    divOne.appendChild(span);
+    div.appendChild(span);
   }
 }
 
-populate();
-// console.log(arr1[randomNumbers()]);
-
-// function shuffle(arr) {
-//   document.querySelectorAll("span").forEach(span => {
-//     span.style.color = "black";
-//   });
-//   let value = arr[randomNumbers()] + 1;
-//   console.log(value);
-//   document.querySelector(".span" + value).style.color = "red";
-// }
+let counter = 0;
+var interval;
+var interval2;
+var interval3;
 
 document.querySelector("button").addEventListener("click", () => {
-  animatedSpin();
-  //   spin(arr1);
-  //   shuffle(arr2);
-  //   shuffle(arr3);
+  interval = setInterval(() => {
+    animatedSpin();
+  }, 20);
+  interval2 = setInterval(() => {
+    animatedSpin2();
+  }, 20);
+  interval3 = setInterval(() => {
+    animatedSpin3();
+  }, 20);
+  document.querySelector("button").style.display = "none";
+  document.querySelector("button.stop").style.display = "block";
 });
 
+document.querySelector("button.stop").addEventListener("click", () => {
+  clearInterval(interval);
+  clearSecondInterval();
+  clearThirdInterval();
+  document.querySelector("button").style.display = "block";
+  document.querySelector("button.stop").style.display = "none";
+  didIWin();
+});
+
+function clearSecondInterval() {
+  setTimeout(() => {
+    clearInterval(interval2);
+  }, 500);
+}
+
+function clearThirdInterval() {
+  setTimeout(() => {
+    let picked1 = document.querySelector(".div1 .span3").textContent;
+    let picked2 = document.querySelector(".div2 .span3").textContent;
+    let picked3 = document.querySelector(".div3 .span3").textContent;
+    console.log("wylosowałeś: " + picked1 + " " + picked2 + " " + picked3);
+  }, 2000);
+}
+
+function didIWin() {
+  setTimeout(() => {
+    clearInterval(interval3);
+  }, 1000);
+}
+
 function animatedSpin() {
-  // document.querySelectorAll(".div1 span").forEach(span => {
-  //     span.classList.add("spin");
-  //     span.addEventListener("animationend", spin(arr1));
-  //   });
+  counter = 1;
   document.querySelector(".div1").classList.add("spin");
   document.querySelector(".spin").addEventListener("animationend", endSpin);
 }
 
+function animatedSpin2() {
+  counter = 1;
+  document.querySelector(".div2").classList.add("spin2");
+  document.querySelector(".spin2").addEventListener("animationend", endSpin2);
+}
+
+function animatedSpin3() {
+  counter = 1;
+  document.querySelector(".div3").classList.add("spin3");
+  document.querySelector(".spin3").addEventListener("animationend", endSpin3);
+}
+
 function endSpin() {
-  spin(arr1);
-  console.log("stop");
+  spin(arr1, 1);
   document
     .querySelector(".spin")
     .removeEventListener("animationend", endSpin, true);
   document.querySelector(".div1").classList.remove("spin");
 }
 
-function spin(arr) {
+function endSpin2() {
+  spin(arr2, 2);
+  document
+    .querySelector(".spin2")
+    .removeEventListener("animationend", endSpin, true);
+  document.querySelector(".div2").classList.remove("spin2");
+}
+
+function endSpin3() {
+  spin(arr3, 3);
+  document
+    .querySelector(".spin3")
+    .removeEventListener("animationend", endSpin, true);
+  document.querySelector(".div3").classList.remove("spin3");
+}
+
+function spin(arr, nr) {
+  console.log(arr);
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] == 5) {
       arr[i] = 0;
     } else {
       arr[i] = arr[i] + 1;
     }
-    document.querySelectorAll("span")[i].innerHTML = arr[i];
+    document.querySelector(".div" + nr + " .span" + i).innerHTML = arr[i];
   }
 }
-
-// let height = 0;
-// let customers = 0;
-// // const array = [];
-// // let i = 0;
-// // for (i = 40; i > 0; i--) {
-// //   array.push(Math.floor(Math.random() * 36));
-// // }
-
-// // console.log(array);
-
-// function getHeight() {
-//   for (i = arr1.length - 1; i > -1; i--) {
-//     height = arr1[i];
-//     document.querySelectorAll("div")[i].style.height = height + "px";
-//     document.querySelectorAll("span")[i].innerHTML = array[i];
-//     if (i < 13) {
-//       document.querySelectorAll("div")[i].style.opacity = 0.5;
-//     } else if (i < 26) {
-//       document.querySelectorAll("div")[i].style.opacity = 0.7;
-//     }
-//     if (height > 130) {
-//       document.querySelectorAll("span")[i].style.opacity = 1;
-//     }
-//   }
-// }
-// getHeight();
-
-// function scrollArray() {
-//   for (i = 0; i < 39; i++) {
-//     array[i] = array[i + 1];
-//   }
-//   array.pop();
-//   array.push(Math.floor(Math.random() * 36));
-//   getHeight();
-// }
-// setInterval(scrollArray, 1000);
