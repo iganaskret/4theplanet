9c9303dce2ddef7b806e87c9da9c955c986cdb78
ksawerykarczewski <ksaw0011@stud.kea.dk>
2019-12-04 22:07:30 +0100

    initial morph animation

--- script/script.js
+++ script/script.js
@@ -66,3 +66,21 @@ function submitForm(inputData) {
 function submittingCompleted() {
   document.location.href = "subpage.html";
 }
+
+// MORPH TRANSITIONS
+
+var btn = document.querySelector("#code_btn");
+//var btn2 = document.querySelector("#code_btn2");
+var sect1 = document.querySelector("#sect_1");
+//var sect2 = document.querySelector("#sect_2");
+var overlay = document.querySelector("#morph");
+
+var morphing = anime({
+  targets: ".morph",
+  d: [{ value: "M8734-7299h154.057s469.97.589,956,0,614.614,0,614.614,0H8734Z" }, { value: "M8734-7299l-.184,1079.988h1922.5V-7299Z" }],
+  transform: [{ value: "translate(-9072.979 4587.749)" }, { value: "translate(-8733.816 7299)" }],
+  easing: "easeInOutQuint",
+  duration: 2300,
+  loop: false
+  //autoplay: false
+});
