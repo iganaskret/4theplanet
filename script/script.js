f4a1a4b69c33874c869675efc604366c855fae2c
ksawerykarczewski <ksaw0011@stud.kea.dk>
2019-12-06 10:56:20 +0100

    media queries to fix

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const form = document.querySelector("form#form1");
-form.setAttribute("novalidate", true);
+// form.setAttribute("novalidate", true);
+let postLink = "http://iganaskret.com/wpv1/wp-json/wp/v2/4earth?_embed";
+const template = document.querySelector("template").content;
 
 fetch("https://form4earth-2b74.restdb.io/rest/form4earth", {
   method: "get",
@@ -34,21 +36,7 @@ form.addEventListener("submit", evt => {
   // };
   // submitForm(inputData);
 });
-
-// function submitForm() {
-//   post();
-// }
-
 function submitForm(inputData) {
-  // let data = {
-  //   Name: "Tomek",
-  //   Email: "romek@kea.dk",
-  //   Subscriber: "true"
-  //   // Name: form.elements.fullname.value,
-  //   // Email: form.elements.email.value,
-  //   // Subscriber: "true"
-  // };
-
   let postData = JSON.stringify(inputData);
   fetch("https://form4earth-2b74.restdb.io/rest/form4earth", {
     method: "post",
@@ -94,4 +82,45 @@ btn.addEventListener("click", function() {
   morphing.restart();
   overlay.classList.add("pointer");
   sect2.classList.add("animated", "display", "flipInY", "delay-1s");
+});
+
+// FETCH AND DISPALY FROM WORDPRESS
+
+function loadData() {
+  fetch(postLink)
+    .then(e => e.json())
+    .then(showWpData);
+}
+
+function showWpData(data) {
+  data.forEach(post => {
+    //clone
+    const clone = template.cloneNode(true);
+    //populate
+    const paragraph_title = clone.querySelector(".paragraph_title");
+    const paragraph_text = clone.querySelector(".paragraph_text");
+    const paragraph_img = clone.querySelector(".paragraph_img");
+
+    paragraph_title.textContent = post.title.rendered;
+    paragraph_text.textContent = post.content.rendered;
+    // paragraph_img.src = post._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+    if (post.img != false) paragraph_img.src = post.img.guid;
+
+    // console.log(post);
+
+    //append
+    document.querySelector("." + post.position).appendChild(clone);
+  });
+}
+
+loadData(postLink);
+
+// LOTTIE IPHONE
+
+let animation = lottie.loadAnimation({
+  container: document.querySelector("#svg_landing"), // the dom element that will contain the animation
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "data.json" // the path to the animation json
 });
