20dc35f0f2e5dc9bf0f90202db66b9c44289dd57
ksawerykarczewski <ksaw0011@stud.kea.dk>
2019-12-09 13:35:58 +0100

    newsletter form fixed

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const form = document.querySelector("form#form1");
+const form_code = document.querySelector("form#form_code");
+const form_newsletter = document.querySelector("#form_newsletter");
 // form.setAttribute("novalidate", true);
 let postLink = "http://iganaskret.com/wpv1/wp-json/wp/v2/4earth?_embed";
 const template = document.querySelector("template").content;
@@ -16,16 +17,16 @@ fetch("https://form4earth-2b74.restdb.io/rest/form4earth", {
   .then(e => e.json())
   .then(e => console.log(e));
 
-form.addEventListener("submit", evt => {
+form_code.addEventListener("submit", evt => {
   evt.preventDefault();
 
-  if (form.reportValidity()) {
+  if (form_code.reportValidity()) {
     const inputData = {
-      Name: form.elements.fullname.value,
-      Email: form.elements.email.value,
-      Subscriber: form.elements.subscription.value
+      Name: form_code.elements.fullname.value,
+      Email: form_code.elements.email.value,
+      Subscriber: form_code.elements.subscription.value
     };
-    submitForm(inputData);
+    submitFormCode(inputData);
   } else {
     alert("It's seems like your email is incorrect");
   }
@@ -36,7 +37,7 @@ form.addEventListener("submit", evt => {
   // };
   // submitForm(inputData);
 });
-function submitForm(inputData) {
+function submitFormCode(inputData) {
   let postData = JSON.stringify(inputData);
   fetch("https://form4earth-2b74.restdb.io/rest/form4earth", {
     method: "post",
@@ -66,7 +67,7 @@ function submittingCompleted(inputData) {
 
 // MORPH TRANSITIONS
 
-var btn = document.querySelector("#code_btn");
+var btn_code = document.querySelector("#code_btn");
 var sect1 = document.querySelector("#sect_1");
 var sect2 = document.querySelector("#sect_2");
 var overlay = document.querySelector("#morph");
@@ -82,7 +83,7 @@ var morphing = anime({
   autoplay: false
 });
 
-btn.addEventListener("click", function() {
+btn_code.addEventListener("click", function() {
   morphing.restart();
   overlay.classList.add("pointer");
   sect2.classList.add("animated", "display", "flipInY", "delay-1s");
@@ -128,3 +129,68 @@ let animation = lottie.loadAnimation({
   autoplay: true,
   path: "data.json" // the path to the animation json
 });
+
+// NEWSLETTER - 1step form
+// https://form4earth-2b74.restdb.io/rest/subscribers
+
+const btn_newsletter = document.querySelector(".button3");
+
+const email_input = document.querySelector('input[name="email"]');
+const submit_btn = document.querySelector(".submit");
+
+btn_newsletter.addEventListener("click", function() {
+  btn_newsletter.classList.add("circle-in");
+  btn_newsletter.innerHTML = " ";
+});
+
+btn_newsletter.addEventListener("animationend", function() {
+  form_newsletter.classList.remove("hide");
+  email_input.classList.add("oval-in");
+  submit_btn.classList.add("oval-in-submit");
+  btn_newsletter.classList.add("hide");
+});
+
+form_newsletter.addEventListener("submit", evt => {
+  evt.preventDefault();
+
+  if (form_newsletter.reportValidity()) {
+    const inputData = {
+      Email: form_newsletter.elements.email.value
+    };
+    console.log(inputData);
+    submitFormSubscribtion(inputData);
+  } else {
+    alert("It's seems like your email is incorrect");
+  }
+  // const inputData = {
+  //   Name: form.elements.fullname.value,
+  //   Email: form.elements.email.value,
+  //   Subscriber: form.elements.subscription.value
+  // };
+  // submitForm(inputData);
+});
+function submitFormSubscribtion(inputData) {
+  let postData = JSON.stringify(inputData);
+  console.log(postData);
+  fetch("https://form4earth-2b74.restdb.io/rest/subscribers", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5de4e9004658275ac9dc2184",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(res => res.json())
+    .then(inputData => submittingCompletedNewsletter(inputData));
+  // .then(inputData => submittingCompletedNewsletter(inputData));
+}
+function submittingCompletedNewsletter(inputData) {
+  console.log(inputData);
+  if (inputData.status == 400) {
+    alert("Your email is already in our database!");
+  } else {
+    alert("Well done");
+    form_newsletter.classList.add("hide");
+  }
+}
