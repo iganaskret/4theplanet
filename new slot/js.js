a8d1740ed87761cb2e1384468139147e8e2ce737
Iga <iga.naskret@gmail.com>
2019-12-16 11:07:47 +0100

    kogo ja probuje nabrac, nadal nie dziala

--- new slot/js.js
+++ new slot/js.js
@@ -13,70 +13,21 @@ function loadSVG(data) {
 
 function main() {}
 
-const arr1 = [0, 1, 2, 3, 4, 5];
-const arr2 = [0, 1, 2, 3, 4, 5];
-const arr3 = [0, 1, 2, 3, 4, 5];
-
 function randomNumbers() {
   return Math.floor(Math.random() * 30);
 }
 
-let divOne = document.querySelector("#ir1");
-let divTwo = document.querySelector("#ir2");
-let divThree = document.querySelector("#ir3");
-
-// populate(divOne, 1);
-// populate(divTwo, 2);
-// populate(divThree, 3);
-
-// function populate(div, nr) {
-//   for (let i = 0; i < 6; i++) {
-//     let span = document.createElement("img");
-//     if (nr == 1) {
-//       span.src = "icons/gamico" + i + ".png";
-//     } else if (nr == 2) {
-//       let j = i + 1;
-//       if (j == 6) {
-//         j = 0;
-//       }
-//       span.src = "icons/gamico" + j + ".png";
-//     } else if (nr == 3) {
-//       let j = i - 1;
-//       if (j == -1) {
-//         j = 5;
-//       }
-//       span.src = "icons/gamico" + j + ".png";
-//     }
-//     span.classList.add("span" + i);
-//     div.appendChild(span);
-//   }
-// }
+let columnOne = document.querySelector("#ir1");
+let columnTwo = document.querySelector("#ir2");
+let columnThree = document.querySelector("#ir3");
 
 let counter = 0;
-var interval;
-var interval2;
-var interval3;
 
 document.querySelector("#lever").addEventListener("click", () => {
   console.log("click");
-  interval = setInterval(() => {
-    animatedSpin();
-  }, 20);
-  interval2 = setInterval(() => {
-    animatedSpin2();
-  }, 20);
-  interval3 = setInterval(() => {
-    animatedSpin3();
-  }, 20);
-  setTimeout(stopSpinning, 100 * randomNumbers());
+  for (let i = 0; i < 21; i++)
+    document.querySelector("#ir1").style.transform = `translateY(${i}%)`;
 });
-
-function stopSpinning() {
-  clearInterval(interval);
-  clearSecondInterval();
-  // clearThirdInterval();
-  didIWin();
-}
 
 // document.querySelector("button.stop").addEventListener("click", () => {
 //   clearInterval(interval);
@@ -86,12 +37,6 @@ function stopSpinning() {
 //   document.querySelector("button.stop").style.display = "none";
 //   didIWin();
 // });
-
-function clearSecondInterval() {
-  setTimeout(() => {
-    clearInterval(interval2);
-  }, 500);
-}
 
 // function clearThirdInterval() {
 //   setTimeout(() => {
@@ -106,53 +51,53 @@ function clearSecondInterval() {
 //   }, 2000);
 // }
 
-function didIWin() {
-  setTimeout(() => {
-    clearInterval(interval3);
-  }, 1000);
-}
+// function didIWin() {
+//   setTimeout(() => {
+//     clearInterval(interval3);
+//   }, 1000);
+// }
 
-function animatedSpin() {
-  counter = 1;
-  document.querySelector("#ir1").classList.add("spin");
-  document.querySelector(".spin").addEventListener("animationend", endSpin);
-}
+// function animatedSpin() {
+//   counter = 1;
+//   document.querySelector("#ir1").classList.add("spin");
+//   document.querySelector(".spin").addEventListener("animationend", endSpin);
+// }
 
-function animatedSpin2() {
-  counter = 1;
-  document.querySelector("#ir2").classList.add("spin2");
-  document.querySelector(".spin2").addEventListener("animationend", endSpin2);
-}
+// function animatedSpin2() {
+//   counter = 1;
+//   document.querySelector("#ir2").classList.add("spin2");
+//   document.querySelector(".spin2").addEventListener("animationend", endSpin2);
+// }
 
-function animatedSpin3() {
-  counter = 1;
-  document.querySelector("#ir3").classList.add("spin3");
-  document.querySelector(".spin3").addEventListener("animationend", endSpin3);
-}
+// function animatedSpin3() {
+//   counter = 1;
+//   document.querySelector("#ir3").classList.add("spin3");
+//   document.querySelector(".spin3").addEventListener("animationend", endSpin3);
+// }
 
-function endSpin() {
-  spin(arr1, 1);
-  document
-    .querySelector(".spin")
-    .removeEventListener("animationend", endSpin, true);
-  document.querySelector("#ir1").classList.remove("spin");
-}
+// function endSpin() {
+//   spin(arr1, 1);
+//   document
+//     .querySelector(".spin")
+//     .removeEventListener("animationend", endSpin, true);
+//   document.querySelector("#ir1").classList.remove("spin");
+// }
 
-function endSpin2() {
-  spin(arr2, 2);
-  document
-    .querySelector(".spin2")
-    .removeEventListener("animationend", endSpin, true);
-  document.querySelector("#ir2").classList.remove("spin2");
-}
+// function endSpin2() {
+//   spin(arr2, 2);
+//   document
+//     .querySelector(".spin2")
+//     .removeEventListener("animationend", endSpin, true);
+//   document.querySelector("#ir2").classList.remove("spin2");
+// }
 
-function endSpin3() {
-  spin(arr3, 3);
-  document
-    .querySelector(".spin3")
-    .removeEventListener("animationend", endSpin, true);
-  document.querySelector("#ir3").classList.remove("spin3");
-}
+// function endSpin3() {
+//   spin(arr3, 3);
+//   document
+//     .querySelector(".spin3")
+//     .removeEventListener("animationend", endSpin, true);
+//   document.querySelector("#ir3").classList.remove("spin3");
+// }
 
 let j = 0;
 
@@ -163,12 +108,12 @@ function spin(arr, nr) {
     } else {
       arr[i] = arr[i] + 1;
     }
-    if (j < 3) {
-      j++;
-    } else {
-      j = -2;
-    }
-    root.style.setProperty("--number", j);
+    // if (j < 3) {
+    //   j++;
+    // } else {
+    //   j = -2;
+    // }
+    // root.style.setProperty("--number", j);
     // document.querySelector(".div" + nr + " .span" + i).src =
     //   "icons/gamico" + arr[i] + ".png";
 
