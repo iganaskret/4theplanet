c55288d098f0f9310f7f1fd64945abe78e782885
Iga <iga.naskret@gmail.com>
2019-12-19 12:45:06 +0100

    Updates

---
+++ precache-manifest.6d076d991c0fbc09d75a5e8bf219e454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e95fc326f5eebcda6d0a636b432b56",
+    "url": "/4theplanet/index.html"
+  },
+  {
+    "revision": "9eb16e600e5956fc7f86",
+    "url": "/4theplanet/static/css/main.1de708c7.chunk.css"
+  },
+  {
+    "revision": "717d8b92b615bf123c46",
+    "url": "/4theplanet/static/js/2.000fc9af.chunk.js"
+  },
+  {
+    "revision": "9eb16e600e5956fc7f86",
+    "url": "/4theplanet/static/js/main.671537c5.chunk.js"
+  },
+  {
+    "revision": "ab2de9cae1671dd8429f",
+    "url": "/4theplanet/static/js/runtime-main.cd4cd7fe.js"
+  }
+]);
